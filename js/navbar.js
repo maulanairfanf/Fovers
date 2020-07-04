620b6c9a65fa474be0a523b8554a0f59e0a4cc18
maulanairfanf <maulanairfanf@gmail.com>
2020-07-04 22:21:55 +0700

    7%

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,59 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const nav = document.querySelectorAll(".sidenav");
-})
+document.addEventListener("DOMContentLoaded", function () {
+
+    const navs = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(navs);
+    loadNav();
+
+    function loadNav() {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                if (this.status !== 200)
+                    return
+                document.querySelectorAll(".topnav").forEach(function (nav) {
+                    nav.innerHTML = xhttp.responseText;
+                });
+
+                document.querySelectorAll(".sidenav a, .topnav a").forEach(function (nav) {
+                    nav.addEventListener("click", function (event) {
+                        const sidenav = document.querySelector(".sidenav");
+                        M.Sidenav.getInstance(sidenav).close();
+                        page = event.target.getAttribute("href").substr(1);
+                        loadPage(page);
+                    });
+                });
+                // } else {
+                //     console.log("error")
+                // }
+            };
+
+        }
+        xhttp.open("GET", "navbar.html", true);
+        xhttp.send();
+    }
+
+
+
+    let page = window.location.hash.substr(1);
+    if (page === "") page = "home";
+    loadPage(page);
+
+    function loadPage(page) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                const content = document.querySelector("#body-render");
+                if (this.status === 200) {
+                    content.innerHTML = xhttp.responseText;
+                } else if (this.status === 404) {
+                    content.innerHTML = "<p>Halaman tidak dapat ditemukan.</p>";
+                } else {
+                    content.innerHTML = "<p>Upss halamana tidak dapat di akses</p>";
+                }
+
+            }
+        }
+        xhttp.open("GET", "pages/" + page + ".html", true);
+        xhttp.send();
+    }
+});
--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,82 @@
 const CACHE_NAME = "v1";
-const assetCache = [
+let assetsCache = [
     "/",
+    "/index.html",
+    "/navbar.html",
     "/pages/home.html",
+    "/pages/favorite.html",
+    "/pages/about.html",
+    // js
+    "/js/materialize.js",
+    "/js/materialize.min.js",
+    "/js/navbar.js",
+    "/js/sw.js",
+    // css
+    "/css/materialize.css",
+    "/css/materialize.min.css",
+    "/css/styles.css",
+    // manifest
     "/manifest.json",
+    // serviceworker
+    "/service-worker.js",
+    // link
     "https://fonts.googleapis.com/icon?family=Material+Icons",
     "https://fonts.gstatic.com/s/materialicons/v52/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
     "https://fonts.gstatic.com/s/materialicons/v53/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0",
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf?v=4.7.0",
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff?v=4.7.0"
+    "/css/font-awesome-4.7.0/css/font-awesome.min.css",
+    // "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+    // "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0",
+    // "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf?v=4.7.0",
+    // "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff?v=4.7.0",
+    // assets
+    "/assets/stadium1.png",
+    "/assets/stadium2.png",
+    "/assets/ball.jpg",
+    "/assets/maulana.jpg"
 ]
+
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(assetsCache);
+        })
+    );
+});
+
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches
+        .match(event.request, {
+            cacheName: CACHE_NAME
+        })
+        .then(function (response) {
+            if (response) {
+                console.log("ServiceWorker : use assets from cache ", response.url);
+                return response;
+            }
+
+            console.log(
+                "ServiceWorker : load assets form server ",
+                event.request.url
+            );
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName != CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
