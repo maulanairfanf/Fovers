8ff61d0c98d9438945b7aacb7c2c851600d7afd5
maulanairfanf <maulanairfanf@gmail.com>
2020-07-22 21:46:33 +0700

    notification

--- js/notification.js
+++ js/notification.js
@@ -1,33 +1,34 @@
-if ("Notification" in window) {
-    requestPermission();
-  } else {
-    console.error("Browser tidak mendukung notifikasi.");
-  }
-
-  function requestPermission() {
-    Notification.requestPermission().then(function (result) {
-      if (result === "denied") {
-        console.log("Fitur notifikasi tidak diijinkan.");
-        return;
-      } else if (result === "default") {
-        console.error("Pengguna menutup kotak dialog permintaan ijin.");
-        return;
-      }
-      
-      console.log("Fitur notifikasi diijinkan.");
-    });
-  }
-
-  function showNotifikasiSederhana() {
-    const title = 'Notifikasi Sederhana';
-    const options = {
-        'body': 'Ini adalah konten notifikasi. \nBisa menggunakan baris baru.',
-    }
-    if (Notification.permission === 'granted') {
-        navigator.serviceWorker.ready.then(function(registration) {
-            registration.showNotification(title, options);
-        });
-    } else {
-        console.error('FItur notifikasi tidak diijinkan.');
-    }
-}
+// if (!('serviceWorker' in navigator)) {
+//       console.log("Service worker tidak didukung browser ini.");
+//     } else {
+//       registerServiceWorker();
+//       requestPermission();
+//     }
+//     // Register service worker
+//     function registerServiceWorker() {
+//       return navigator.serviceWorker.register('sw.js')
+//         .then(function (registration) {
+//           console.log('Registrasi service worker berhasil.');
+//           return registration;
+//         })
+//         .catch(function (err) {
+//           console.error('Registrasi service worker gagal.', err);
+//         });
+//     }
+//     function requestPermission() {
+// if ('Notification' in window) {
+//   Notification.requestPermission().then(function (result) {
+//     if (result === "denied") {
+//       console.log("Fitur notifikasi tidak diijinkan.");
+//       return;
+//     } else if (result === "default") {
+//       console.error("Pengguna menutup kotak dialog permintaan ijin.");
+//       return;
+//     }
+    
+//     navigator.serviceWorker.getRegistration().then(function(reg) {
+//       reg.showNotification('Notifikasi diijinkan!');
+//     });
+//   });
+// }
+//     }
